c899916ea18005c28ff7959bb5c52275526d154d
Singeni, Lazola <lsingeni@fnb.co.za>
2020-09-17 22:41:05 +0200

    commit 2

--- src/main/java/com/lazola/assessment/AssessmentApplication.java
+++ src/main/java/com/lazola/assessment/AssessmentApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +20,37 @@ public class AssessmentApplication {
 
 
 	@GetMapping("/conversions/ktoc")
-	public ResponseEntity<String> convertKtoC(@RequestParam(value = "kelvin")Double kelvin) throws Exception{
+	public ResponseEntity<String> convertKelvintoCelsius(@RequestParam(value = "kelvin")Double kelvin) throws Exception{
 		if(kelvin != null){
-			return new ResponseEntity<>(String.format("Celcius : %.2f",Double.valueOf(kelvin) - 273.15d), HttpStatus.OK);
+			return new ResponseEntity<>(String.format("Celsius : %.2f",Double.valueOf(kelvin) - 273.15D), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>("Please enter a valid kelvin value",HttpStatus.BAD_REQUEST);
 		}
+	}
+
+	@GetMapping("/conversions/ctok")
+	public ResponseEntity<String> convertCelsiustoKelvin(@RequestParam(value = "celsius")Double celsius) throws Exception{
+		if(celsius != null){
+			return new ResponseEntity<>(String.format("Kelvin : %.2f", Double.valueOf(celsius) + 273.15D ), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Please enter a valid celsius value", HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	@GetMapping("/conversions/mtok")
+	public ResponseEntity<String> convertMilestoKilometers(@RequestParam(value = "miles") Double miles) throws Exception{
+		if(miles != null){
+			return new ResponseEntity<>(String.format("Kilometers : %.2f", Double.valueOf(miles) * 1.609D ), HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("Please enter a valid miltes value", HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<String> handleMissingParameter(MissingServletRequestParameterException ex){
+		String name = ex.getParameterName();
+		return new ResponseEntity<>(String.format("%s url parameter is missing", name),HttpStatus.BAD_REQUEST);
 	}
 
 }
